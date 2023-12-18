4881d5bd905161ae0ce853ca871128009ae3c0af
Tristan Lépine <tlepine@b2b2c.com>
2023-12-18 15:26:39 -0500

    Added cargo-scaffold to template new days

---
+++ 2023/template/src/main.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod test;
+
+fn process_part_one(input: &str) -> usize {
+    0
+}
+
+fn process_part_two(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    let input = include_str!("../input");
+
+    let part1 = process_part_one(&input);
+    let part2 = process_part_two(&input);
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+}
+
---
+++ 2023/template/src/test.rs
@@ -0,0 +1,14 @@
+const TEST_INPUT: &str = "";
+
+use crate::{process_part_one, process_part_two};
+
+#[test]
+fn part_1() {
+    assert_eq!(process_part_one(TEST_INPUT), 0);
+}
+
+#[test]
+fn part_2() {
+    assert_eq!(process_part_two(TEST_INPUT), 0);
+}
+
