6f65411be86b71527a7c73d0ef7c112c9377d6fe
Tristan Lepine <tristanlepine14@gmail.com>
2025-01-17 22:19:54 -0500

    havent finished day12 :(

--- 2024/day12/src/main.rs
+++ 2024/day12/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::BorrowMut,
     collections::{HashMap, HashSet},
     fs,
     ops::Add,
@@ -49,7 +50,7 @@ impl Coordinate {
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 pub enum Tile {
     /// Represent the number of sides this edge has
     Edge(usize),
@@ -60,18 +61,36 @@ pub enum Tile {
 pub struct Zone {
     plant: char,
     tiles: Vec<Tile>,
+    area: usize,
+    perimeter: usize,
 }
 
 impl Zone {
-    pub fn area(&self) -> usize {
-        self.tiles.len()
+    pub fn new(plant: char, tiles: Vec<Tile>) -> Self {
+        Self {
+            plant,
+            area: Zone::area(&tiles),
+            perimeter: Zone::perimeter(&tiles),
+            tiles,
+        }
     }
 
-    pub fn perimeter(&self) -> usize {
-        self.tiles.iter().fold(0, |acc, s| match s {
+    fn area(tiles: &[Tile]) -> usize {
+        tiles.len()
+    }
+
+    fn perimeter(tiles: &[Tile]) -> usize {
+        tiles.iter().fold(0, |acc, s| match s {
             Tile::Edge(e) => acc + e,
             Tile::Middle => acc,
         })
+    }
+
+    pub fn only_corners(&mut self) {
+        self.tiles.retain(|t| match t {
+            Tile::Edge(s) => s >= &2,
+            Tile::Middle => false,
+        });
     }
 }
 
@@ -122,7 +141,7 @@ pub fn get_zone(
                         edges += 1;
                     }
                 } else {
-                    edges += 1
+                    edges += 1;
                 }
             });
 
@@ -137,10 +156,7 @@ pub fn get_zone(
         }
     }
 
-    Zone {
-        tiles,
-        plant: *our_char,
-    }
+    Zone::new(*our_char, tiles)
 }
 
 fn part_1(input: &str) -> usize {
@@ -156,11 +172,32 @@ fn part_1(input: &str) -> usize {
         zones.push(get_zone(coord, &map, &mut checked));
     }
 
-    zones.into_iter().map(|z| z.area() * z.perimeter()).sum()
+    zones.into_iter().map(|z| z.area * z.perimeter).sum()
 }
 
 fn part_2(input: &str) -> usize {
-    0
+    let map = parse(input);
+    let mut checked: HashSet<Coordinate> = HashSet::new();
+    let mut zones: Vec<Zone> = Vec::new();
+
+    for coord in map.clone().into_keys() {
+        if checked.contains(&coord) {
+            continue;
+        }
+
+        let mut z = get_zone(coord, &map, &mut checked);
+        z.only_corners();
+
+        zones.push(z);
+    }
+
+    zones
+        .into_iter()
+        .inspect(|z| {
+            dbg!((z, Zone::perimeter(&z.tiles) / 2));
+        })
+        .map(|z| z.area * (Zone::perimeter(&z.tiles) / 2))
+        .sum()
 }
 
 // could use the shoelace formula to calculate area & perimeter straight-forward way
@@ -168,8 +205,6 @@ fn part_2(input: &str) -> usize {
 
 #[cfg(test)]
 mod test {
-    use std::collections::{HashMap, HashSet};
-
     use crate::{get_zone, parse, part_1, part_2, Coordinate, Tile, Zone};
 
     const INPUT_TEST: &str = "RRRRIICCFF
@@ -190,66 +225,21 @@ OXOXO
 OOOOO";
 
     #[test]
-    fn can_calculate_area_and_perimeter() {
-        let z1 = Zone {
-            plant: 'R',
-            tiles: vec![Tile::Edge(2), Tile::Edge(2), Tile::Edge(2), Tile::Edge(2)],
-        };
-
-        assert_eq!(z1.area(), 4);
-        assert_eq!(z1.perimeter(), 8);
-
-        let map = parse(INPUT_TEST2);
-        let mut correct: HashMap<Coordinate, char> = HashMap::new();
-
-        correct.insert(Coordinate { x: 0, y: 0 }, 'O');
-        correct.insert(Coordinate { x: 1, y: 0 }, 'O');
-        correct.insert(Coordinate { x: 2, y: 0 }, 'O');
-        correct.insert(Coordinate { x: 3, y: 0 }, 'O');
-        correct.insert(Coordinate { x: 4, y: 0 }, 'O');
-
-        correct.insert(Coordinate { x: 0, y: 1 }, 'O');
-        correct.insert(Coordinate { x: 1, y: 1 }, 'X');
-        correct.insert(Coordinate { x: 2, y: 1 }, 'O');
-        correct.insert(Coordinate { x: 3, y: 1 }, 'X');
-        correct.insert(Coordinate { x: 4, y: 1 }, 'O');
-
-        correct.insert(Coordinate { x: 0, y: 2 }, 'O');
-        correct.insert(Coordinate { x: 1, y: 2 }, 'O');
-        correct.insert(Coordinate { x: 2, y: 2 }, 'O');
-        correct.insert(Coordinate { x: 3, y: 2 }, 'O');
-        correct.insert(Coordinate { x: 4, y: 2 }, 'O');
-
-        correct.insert(Coordinate { x: 0, y: 3 }, 'O');
-        correct.insert(Coordinate { x: 1, y: 3 }, 'X');
-        correct.insert(Coordinate { x: 2, y: 3 }, 'O');
-        correct.insert(Coordinate { x: 3, y: 3 }, 'X');
-        correct.insert(Coordinate { x: 4, y: 3 }, 'O');
-
-        correct.insert(Coordinate { x: 0, y: 4 }, 'O');
-        correct.insert(Coordinate { x: 1, y: 4 }, 'O');
-        correct.insert(Coordinate { x: 2, y: 4 }, 'O');
-        correct.insert(Coordinate { x: 3, y: 4 }, 'O');
-        correct.insert(Coordinate { x: 4, y: 4 }, 'O');
-
-        assert_eq!(map, correct);
-
-        let z1 = get_zone(Coordinate { x: 0, y: 0 }, &map, &mut HashSet::new());
-
-        dbg!(&z1);
-
-        assert_eq!(z1.area(), 21);
-        assert_eq!(z1.perimeter(), 36);
-    }
-
-    #[test]
     fn solve_part_1() {
         assert_eq!(part_1(INPUT_TEST2), 772);
         assert_eq!(part_1(INPUT_TEST), 1930);
     }
 
+    const INPUT_TEST3: &str = "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA";
+
     #[test]
     fn solve_part_2() {
-        assert_eq!(part_2(INPUT_TEST), 1);
+        assert_eq!(part_2(INPUT_TEST), 1206);
+        assert_eq!(part_2(INPUT_TEST2), 436);
     }
 }
