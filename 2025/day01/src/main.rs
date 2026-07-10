b6db94f7d9b17bbb0edf69b09be355bd009960ac
Tristan Lepine <tristan.lepine@ebox.ca>
2026-07-10 18:59:25 +0000

    feat: finished part 2

--- 2025/day01/src/main.rs
+++ 2025/day01/src/main.rs
@@ -31,13 +31,38 @@ fn part_1(input: &str) -> usize {
     count.try_into().unwrap()
 }
 
+fn part_2(input: &str) -> usize {
+    let mut count: isize = 0;
+    let mut dial: isize = 50;
+    input.lines().for_each(|l| {
+        let ticks: isize = l[1..].parse().unwrap();
+
+        if l.chars().nth(0) == Some('L') {
+            let correct_position = (dial - 1).rem_euclid(100) + 1;
+            count += (ticks - correct_position + 100) / 100;
+            dial = (dial - ticks).rem_euclid(100);
+        } else {
+            count += (ticks + dial) / 100;
+            dial = (dial + ticks).rem_euclid(100);
+        }
+    });
+
+    count.try_into().unwrap()
+}
+
 #[test]
 fn test_part_1() {
     assert_eq!(part_1(TEST_INPUT), 3);
 }
 
+#[test]
+fn test_part_2() {
+    assert_eq!(part_2(TEST_INPUT), 6);
+}
+
 fn main() {
     let input = fs::read_to_string("src/input.txt").unwrap();
-    
+
     println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
